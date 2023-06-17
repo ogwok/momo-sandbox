4e77c7e98950cd249b3f6bfdfd5ff99604ca158b
ogwok <williamogwok10@gmail.com>
2023-06-17 18:15:19 +0300

    fix axios returning 401 error

--- index.js
+++ index.js
@@ -16,66 +16,48 @@ class MomoSandbox {
     return uuidv4();
   }
 
-  async createApiUser(UUID) {
-    try {
-      const options = {
-        method: 'POST',
-        url: 'https://sandbox.momodeveloper.mtn.com/v1_0/apiuser',
-        headers: {
-          'X-Reference-Id': UUID,
-          'Content-Type': 'application/json',
-          'Ocp-Apim-Subscription-Key': this.primaryKey,
-        },
-        data: {
-          providerCallbackHost: this.callbackHost,
-        },
-      };
+  createApiUser(UUID) {
+    const options = {
+      method: 'POST',
+      url: 'https://sandbox.momodeveloper.mtn.com/v1_0/apiuser',
+      headers: {
+        'X-Reference-Id': UUID,
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': this.primaryKey,
+      },
+      data: {
+        providerCallbackHost: this.callbackHost,
+      },
+    };
 
-      await axios(options);
-    } catch (error) {
-      throw new Error(`Failed to create API user: ${error.message}`);
-    }
+    return axios(options);
   }
 
-  async confirmApiUser(UUID) {
-    try {
-      const options = {
-        method: 'GET',
-        url: `https://sandbox.momodeveloper.mtn.com/v1_0/apiuser/${UUID}`,
-        headers: {
-          'X-Reference-Id': UUID,
-          'Ocp-Apim-Subscription-Key': this.primaryKey,
-        },
-      };
+  confirmApiUser(UUID) {
+    const options = {
+      method: 'GET',
+      url: `https://sandbox.momodeveloper.mtn.com/v1_0/apiuser/${UUID}`,
+      headers: {
+        'X-Reference-Id': UUID,
+        'Ocp-Apim-Subscription-Key': this.primaryKey,
+      },
+    };
 
-      await axios(options);
-    } catch (error) {
-      throw new Error(`Failed to confirm API user: ${error.message}`);
-    }
+    return axios(options);
   }
 
-  async createUserApiKey(UUID) {
-    try {
-      const options = {
-        method: 'POST',
-        url: `https://sandbox.momodeveloper.mtn.com/v1_0/apiuser/${UUID}/apikey`,
-        headers: {
-          'X-Reference-Id': UUID,
-          'Ocp-Apim-Subscription-Key': this.primaryKey,
-        },
-      };
+  createUserApiKey(UUID) {
+    const options = {
+      method: 'POST',
+      url: `https://sandbox.momodeveloper.mtn.com/v1_0/apiuser/${UUID}/apikey`,
+      headers: {
+        'X-Reference-Id': UUID,
+        'Ocp-Apim-Subscription-Key': this.primaryKey,
+      },
+    };
 
-      const response = await axios(options);
-      const userApiKey = response.data.apiKey;
-
-      if (userApiKey) {
-        return userApiKey;
-      } else {
-        throw new Error('Failed to create user API key');
-      }
-    } catch (error) {
-      throw new Error(`Failed to create user API key: ${error.message}`);
-    }
+    return axios(options)
+      .then(response => response.data.apiKey);
   }
 
   async generateUserCredentials() {
@@ -91,17 +73,23 @@ class MomoSandbox {
       console.log('userApiKey:', this.userApiKey);
       console.log('callbackHost:', this.callbackHost);
     } catch (error) {
-      console.error('An error occurred:', error.message);
+      console.error('An error occurred:', error);
     }
   }
 }
 
 program
-  .requiredOption('--callback-host <callbackHost>', 'The callback host')
-  .requiredOption('--primary-key <key>', 'The primary key')
+  .option('--callback-host <callbackHost>', 'The callback host')
+  .option('--primary-key <key>', 'The primary key')
   .parse(process.argv);
 
-const { callbackHost, primaryKey } = program;
+const { callbackHost, primaryKey } = program.opts();
+
+if (!callbackHost || !primaryKey) {
+  console.error('Please provide the callback host and primary key.');
+  program.help();
+  process.exit(1);
+}
 
 const momoSandbox = new MomoSandbox(callbackHost, primaryKey);
 momoSandbox.generateUserCredentials();
